b1be5ce5647edeabbf64f12a93034bd087e6abfc
zhcui <zcui@caltech.edu>
2018-06-29 02:39:16 -0700

    add test for ufunc

--- sparse/tests/test_ufunc.py
+++ sparse/tests/test_ufunc.py
@@ -9,20 +9,27 @@ from sparse.utils import assert_eq
 
 #import pytest
 
-def test_add():
-    #x = sparse.brandom((4, 2, 6), (2, 1, 2), 0.5, format='bcoo')
-    #y = x.todense()
+def test_add_simple():
     data = np.arange(1,4).repeat(4).reshape(3,2,2)
     coords = np.array([[1,1,0],[0,1,1]])
     x = BCOO(coords, data = data, shape = (4,4), block_shape = (2,2))
-    #print x
-    #print x.todense()
-    #'''
+    x_d = x.todense()
     y = x + x
-    print y
-    print y.todense()
-    #'''
+    y_d = x_d + x_d
+    assert_eq(y, y_d)
+
+def test_add_multi_dim_array():
+    x = sparse.brandom((4, 2, 6), (2, 1, 2), 0.5, format='bcoo')
+    x_d = x.todense()
+    y = sparse.brandom((4, 2, 6), (2, 1, 2), 0.5, format='bcoo')
+    y_d = y.todense()
+    
+    z = x + y
+    z_d = x_d + y_d
+    assert_eq(z, z_d)
+
 
 if __name__ == '__main__':
     print("\n main test \n")
-    test_add()
+    test_add_simple()
+    test_add_multi_dim_array()
